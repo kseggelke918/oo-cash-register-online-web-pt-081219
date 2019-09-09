b5e6633745069cafe65e3d9a36df7d6a90e75a43
Katie Seggelke <katie.seggelke@gmail.com>
2019-09-09 17:14:29 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -11,7 +11,7 @@ class CashRegister
 
   def add_item(title, price, quantity = 1)
     @total += (price * quantity) 
-    @last_transaction = @total 
+    @last_transaction = price * quantity 
     if quantity > 1
       i = 0 
       while i < quantity
